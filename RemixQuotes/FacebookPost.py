1be224266137af1ef085f3e16e5e41bd3ff6aac0
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 16:06:12 +0530

    fix: modernize code + rewrite README

--- RemixQuotes/FacebookPost.py
+++ RemixQuotes/FacebookPost.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jan 20 01:18:34 2018
+Facebook Quote Poster
+
+Posts random quotes to Facebook using Graph API.
 
 @author: kaustabh
 """
+import os
 from facepy import GraphAPI
 from webscrapper import Quotes
 
-#enter your access_token inside the quotes from https://developers.facebook.com/tools/explorer
-ACCESS_TOKEN = '' 
 
-web = Quotes() 
-graph = GraphAPI(ACCESS_TOKEN)
+def main():
+    # Get access token from environment variable or prompt user
+    access_token = os.getenv('FB_ACCESS_TOKEN', '')
+    
+    if not access_token:
+        print("Please set FB_ACCESS_TOKEN environment variable or edit this file")
+        print("Get your token from: https://developers.facebook.com/tools/explorer")
+        return
+    
+    try:
+        web = Quotes() 
+        graph = GraphAPI(access_token)
+        
+        message = f'"{web.randomquote()}"\nBy: {web.get_author()}'
+        
+        result = graph.post('me/feed', message=message)
+        print(f"Successfully posted quote! Post ID: {result.get('id', 'Unknown')}")
+        
+    except Exception as e:
+        print(f"Error posting to Facebook: {e}")
 
-message = "\" "+web.randomquote()+"\""+"\n By : "+web.author
 
-graph.post('me/feed', message = message)
+if __name__ == "__main__":
+    main()
 
